9fb060a5e1777cd93cae0f696598b99d7e4f9e76
groma19 <roman.ghughunishvili@gmail.com>
2026-07-22 17:07:32 +0400

    added raylib.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main(void) {
-    printf("Hello, world!\n");
-    return 0;
+  InitWindow(800, 450, "raylib example - basic window");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! You created your first window!", 190, 200, 20,
+             LIGHTGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
